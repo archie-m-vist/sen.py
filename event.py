e2a13397b193d5e6a9437cb378f91e8f810abfe8
Archie M. Vist <archie.m.vist@gmail.com>
2018-02-12 23:24:37 -0500

    Added reason field to ClockStoppedEvent per SENPAI updates.

--- event.py
+++ event.py
@@ -64,7 +64,8 @@ class ClockStartedEvent (ClockEvent):
       return output
 
 class ClockStoppedEvent (ClockEvent):
-   def __init__ (self, *args, **kwargs):
+   def __init__ (self, reason, *args, **kwargs):
+      self.reason = reason
       super().__init__(*args,**kwargs)
    
    def __str__ (self):
